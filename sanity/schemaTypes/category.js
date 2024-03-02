dbce2599b5cdc4791fbb480c319e1a9edadfabbb
Palak Goyal <Palakgoyal0304@gmail.com>
2024-03-02 08:02:11 +0000

    create sanity schema

--- sanity/schemaTypes/category.js
+++ sanity/schemaTypes/category.js
@@ -1,19 +1,22 @@
 import {defineField, defineType} from 'sanity'
+import { defineCliConfig } from "@sanity/cli"
 
 export default defineType({
   name: 'category',
-  title: 'Category',
+  title: 'Menu Category',
   type: 'document',
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
+      
+      name: 'name',
+      title: 'Category Name',
       type: 'string',
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      name: 'image',
+      type: 'image',
+      title: 'Image of category',
     }),
   ],
 })
